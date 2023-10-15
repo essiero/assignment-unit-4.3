6286b52e0af198863edf17ea53786fd54353e672
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-15 18:51:01 -0500

    finished

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,13 +2,32 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+addItem(4);
+addItem(8);
+addItem(525600);
+console.log(basket);
 
+function listItems(array){
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }
+}
+console.log(listItems(basket));
+console.log(basket);
 
+function empty(array){
+    array.pop();
+    console.log(array);
+    return true;
+}
 
-
-
-
-
+empty(basket);
+console.log(basket);
 
 
 
